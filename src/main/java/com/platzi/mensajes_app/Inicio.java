6000df9d6ccadc46ad9353edb7b5a97d73935d85
Dennis David Arango <45930005+dennisdavidarango@users.noreply.github.com>
2020-04-24 12:34:30 -0500

    Aplicación con 3 nuevas clases y lista para conectarse con DAO

--- src/main/java/com/platzi/mensajes_app/Inicio.java
+++ src/main/java/com/platzi/mensajes_app/Inicio.java
@@ -6,6 +6,7 @@
 package com.platzi.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -15,13 +16,39 @@ public class Inicio {
     
     public static void main(String[] args) {
         
-        Conexion conexion = new Conexion();
+        Scanner sc = new Scanner(System.in);
         
-        try(Connection cnx = conexion.get_connection()){
+        int opcion=0;
         
-        }catch(Exception e){
-            System.out.println(e);
-        }
+        do{
+            System.out.println("------------------");
+            System.out.println("Aplicación de mensajes");
+            System.out.println("1. Crear un mensaje");
+            System.out.println("2. Listar mensajes");
+            System.out.println("3. Editar mensaje");
+            System.out.println("4. Elimiar mensaje");
+            System.out.println("5. Salir");
+            opcion = sc.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3:
+                    MensajesService.editarMensaje();
+                    break;
+                case 4:
+                    MensajesService.borrarMensaje();
+                    
+                    break;
+                default:
+                    break;
+            }
+            
+        }while(opcion != 5);
+        
     }
-    
 }
---
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.mensajes_app;
+
+/**
+ *
+ * @author JULIMEL
+ */
+public class MensajesDAO {
+    
+    public static void crearMensajeDB(Mensajes mensaje){
+    
+    }
+    
+    public static void leerMensajesDB(){
+    
+    }
+    
+    public static void borrarMensajesDB(int id_mensaje){
+    
+    }
+    
+    public static void actualizarMensajeDB(Mensajes mensaje){
+    
+    }
+    
+}
